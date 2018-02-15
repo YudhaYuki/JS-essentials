77ebdc376d61e2fee51806d82b251b95fc6021d6
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-02-15 16:25:52 +0100

    FUNCTION or SUBROUTINE

--- myapp.js
+++ myapp.js
@@ -1,3 +1,6 @@
+/////////////////--------- DUBUGGING --------------/////////////////
+
+/*
 console.log( "Hello world" );
 
 console.log( 200 );
@@ -11,3 +14,25 @@ var num = 10;
 console.log( num );
 
 console.log( "string", 10.11, true, false, null, num );
+*/
+
+
+
+/////////////////--------- FUNCTION or SUBROUTINE --------------/////////////////
+
+function makeCoffee(sugar, milk) 
+{
+    var instructions = "Boil water,";
+
+    instructions += " pour into cup,";
+
+    instructions += " add coffee granules,";
+
+    instructions += "add " + sugar + " spoons of sugar,";
+
+    instructions += " add " + milk + "% milk.";
+
+    return instructions;
+}
+
+console.log(makeCoffee(20, 50));
